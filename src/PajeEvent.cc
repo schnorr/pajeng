83d7d56541010604054523f82f17932a3904d641
schnorr <Lucas.Schnorr@imag.fr>
2012-03-09 02:54:40 +0100

    error checking when constructing a PajeEvent

--- src/PajeEvent.cc
+++ src/PajeEvent.cc
@@ -2,6 +2,13 @@
 
 PajeEvent::PajeEvent (PajeEventDefinition *def, paje_line *line)
 {
+  if (line->word_count != def->fieldCount){
+    fprintf (stderr, "Field count (%d) does not match definition (%d).\n",
+             line->word_count, def->fieldCount);
+    fprintf (stderr, "%s %d TODO\n", __FILE__, __LINE__);
+    exit(1);
+  }
+
   valueLine = line;
   pajeEventDefinition = def;
 }
