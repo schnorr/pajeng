0631f663b1b9cbcb2e3e613fa44a4ad0d1535569
schnorr <Lucas.Schnorr@imag.fr>
2012-03-12 15:43:19 +0100

    better error report of number of fields

--- src/PajeEvent.cc
+++ src/PajeEvent.cc
@@ -3,10 +3,14 @@
 PajeEvent::PajeEvent (PajeEventDefinition *def, paje_line *line)
 {
   if (line->word_count != def->fieldCount){
-    fprintf (stderr, "Field count (%d) does not match definition (%d).\n",
-             line->word_count, def->fieldCount);
-    fprintf (stderr, "%s %d TODO\n", __FILE__, __LINE__);
-    exit(1);
+    int i;
+    std::string str;
+    for (i = 0; i < line->word_count; i++){
+      str += std::string(line->word[i]);
+      str += " ";
+    }
+    std::cout << *def << std::endl;
+    throw "Field count does not match definition for line '"+ str + "'.";
   }
 
   valueLine = line;
