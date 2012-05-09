5af8ab56ea71a2dcee350958d2879065cfbefbe2
schnorr <Lucas.Schnorr@imag.fr>
2012-05-09 13:12:29 +0200

    split file

---
+++ src/viva/graphframe.h
@@ -0,0 +1,46 @@
+#ifndef __GRAPH_FRAME_H
+#define __GRAPH_FRAME_H
+#include <wx/wx.h>
+#include "basic.h"
+
+class VivaGraph;
+
+class GraphFrame : public BasicFrame
+{
+public:
+  GraphFrame () {;};
+  GraphFrame (wxWindow *parent,
+              wxWindowID id,
+              const wxPoint & pos = wxDefaultPosition,
+              const wxSize & size = wxDefaultSize,
+              long style = wxDEFAULT_FRAME_STYLE,
+              const wxString & name = _T("GraphFrame"));
+  ~GraphFrame () {;};
+  bool Create(wxWindow* parent,
+              wxWindowID id,
+              const wxPoint & pos = wxDefaultPosition,
+              const wxSize & size = wxDefaultSize,
+              long style = wxDEFAULT_FRAME_STYLE,
+              const wxString & name = _T("GraphFrame"));
+  void setVivaGraph (VivaGraph *vivagraph);
+
+private:
+  VivaGraph *vivagraph;
+
+  void OnPaint (wxPaintEvent& event);
+
+  //user interactions
+  void leftMouseClicked (wxMouseEvent& event);
+  void rightMouseClicked (wxMouseEvent& event);
+
+  //from VivaGraph, and runner
+  void OnVivaGraphChanged (wxCommandEvent& event);
+  void OnVivaGraphLayoutUpdated (wxCommandEvent& event);
+
+protected:
+  wxSize DoGetBestSize ();
+
+};
+
+#include "VivaGraph.h"
+#endif
--- src/viva/graphview.h
+++ src/viva/graphview.h
@@ -2,51 +2,13 @@
 #define __GRAPH_WINDOW_H
 #include <wx/artprov.h>
 #include "basic.h"
-#include "timeinterval.h"
+#include "graphframe.h"
 #include "VivaGraph.h"
 
 class VivaGraph;
-class TimeIntervalView;
 
 DECLARE_EVENT_TYPE (VivaGraphChanged, -1)
 DECLARE_EVENT_TYPE (VivaGraphLayoutUpdated, -1)
-
-class GraphFrame : public BasicFrame
-{
-public:
-  GraphFrame () {;};
-  GraphFrame (wxWindow *parent,
-              wxWindowID id,
-              const wxPoint & pos = wxDefaultPosition,
-              const wxSize & size = wxDefaultSize,
-              long style = wxDEFAULT_FRAME_STYLE,
-              const wxString & name = _T("GraphFrame"));
-  ~GraphFrame () {;};
-  bool Create(wxWindow* parent,
-              wxWindowID id,
-              const wxPoint & pos = wxDefaultPosition,
-              const wxSize & size = wxDefaultSize,
-              long style = wxDEFAULT_FRAME_STYLE,
-              const wxString & name = _T("GraphFrame"));
-  void setVivaGraph (VivaGraph *vivagraph);
-
-private:
-  VivaGraph *vivagraph;
-
-  void OnPaint (wxPaintEvent& event);
-
-  //user interactions
-  void leftMouseClicked (wxMouseEvent& event);
-  void rightMouseClicked (wxMouseEvent& event);
-
-  //from VivaGraph, and runner
-  void OnVivaGraphChanged (wxCommandEvent& event);
-  void OnVivaGraphLayoutUpdated (wxCommandEvent& event);
-
-protected:
-  wxSize DoGetBestSize ();
-
-};
 
 class GraphWindow : public wxFrame
 {
