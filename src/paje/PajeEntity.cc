615bca448979dfa1f92c9b0efbb7afff0f778f5b
schnorr <Lucas.Schnorr@imag.fr>
2012-05-21 15:58:31 +0200

    adding new class PajeEntity that will be the root class for all Paje entities (container, variable, state, event, link)

---
+++ src/paje/PajeEntity.cc
@@ -0,0 +1,148 @@
+#include "PajeEntity.h"
+
+PajeEntity::PajeEntity (PajeContainer *container, PajeType *type, std::string name)
+{
+  this->container = container;
+  this->type = type;
+  this->name = name;
+}
+
+bool PajeEntity::isContainer (void)
+{
+  throw "Should be implemented in subclasses";
+  return false;
+}
+
+std::string PajeEntity::value (void)
+{
+  throw "Should be implemented in subclasses";
+  return std::string();
+}
+
+PajeContainer *PajeEntity::startContainer (void)
+{
+  return container;
+}
+
+PajeContainer *PajeEntity::endContainer (void)
+{
+  return container;
+}
+
+bool PajeEntity::isContainedBy (PajeContainer *container)
+{
+  return this->container == container;
+}
+
+double PajeEntity::time (void)
+{
+  throw "Should be implemented in subclasses";
+  return 0;
+}
+
+double PajeEntity::startTime (void)
+{
+  return time();
+}
+
+double PajeEntity::endTime (void)
+{
+  return time();
+}
+
+double PajeEntity::firstTime (void)
+{
+  return startTime () < endTime() ? startTime() : endTime();
+}
+
+double PajeEntity::lastTime (void)
+{
+  return startTime () > endTime() ? startTime() : endTime();
+}
+
+double PajeEntity::duration (void)
+{
+  return endTime() - startTime();
+}
+
+double PajeEntity::exclusiveDuration (void)
+{
+  return duration();
+}
+
+double PajeEntity::doubleValue (void)
+{
+  return 0;
+}
+
+double PajeEntity::minValue (void)
+{
+  return 0;
+}
+
+double PajeEntity::maxvalue (void)
+{
+  return 0;
+}
+
+PajeUserEvent::PajeUserEvent (PajeContainer *container, PajeType *type, std::string name, PajeEvent *event):PajeEntity(container, type, name)
+{
+}
+
+PajeUserVariable::PajeUserVariable (PajeContainer *container, PajeType *type, double value, double startTime, double endTime):PajeEntity (container, type, "")
+{
+  this->val = value;
+  this->stime = startTime;
+  this->etime = endTime;
+}
+
+double PajeUserVariable::startTime (void)
+{
+  return stime;
+}
+
+double PajeUserVariable::endTime (void)
+{
+  return etime;
+}
+
+double PajeUserVariable::time (void)
+{
+  return stime;
+}
+
+double PajeUserVariable::doubleValue (void)
+{
+  return val;
+}
+
+double PajeUserVariable::maxValue (void)
+{
+  return val;
+}
+
+double PajeUserVariable::minValue (void)
+{
+  return val;
+}
+
+void PajeUserVariable::setEndTime (double endTime)
+{
+  this->etime = endTime;
+}
+
+PajeUserState::PajeUserState (PajeContainer *container, PajeType *type, std::string value, PajeEvent *startEvent):PajeUserEvent(container, type, value, startEvent)
+{
+}
+
+PajeUserLink::PajeUserLink (PajeContainer *container, PajeType *type, std::string value, std::string key, PajeContainer *startContainer, PajeEvent *startEvent):PajeUserState(container, type, value, startEvent)
+{
+  this->key = key;
+  this->startContainer = startContainer;
+}
+
+// PajeUserLink::PajeUserLink (PajeContainer *container, PajeType *type, std::string value, std::string key, PajeContainer *endContainer, PajeEvent *endEvent)
+// {
+//   this->key = key;
+//   this->endContainer = endContainer;
+// }
---
+++ src/paje/PajeEntity.h
@@ -0,0 +1,92 @@
+#ifndef __PAJE_ENTITY_H
+#define __PAJE_ENTITY_H
+#include "PajeObject.h"
+#include "PajeType.h"
+#include "PajeEvent.h"
+
+class PajeContainer;
+
+class PajeEntity : public PajeObject
+{
+public:
+  PajeContainer *container;
+  PajeType *type;
+  std::string name;
+
+public:
+  PajeEntity (PajeContainer *container, PajeType *type, std::string name);
+ 
+  bool virtual isContainer (void);
+  std::string virtual value (void);
+  PajeContainer virtual *startContainer (void);
+  PajeContainer virtual *endContainer (void);
+  bool virtual isContainedBy (PajeContainer *container);
+
+  double virtual time (void);
+  double virtual startTime (void);
+  double virtual endTime (void);
+  double virtual firstTime (void);
+  double virtual lastTime (void);
+  double virtual duration (void);
+  double virtual exclusiveDuration (void);
+
+  double virtual doubleValue (void);
+  double virtual minValue (void);
+  double virtual maxvalue (void);
+};
+
+class PajeUserEvent : public PajeEntity
+{
+public:
+  std::map<std::string,std::string> extraFields;
+  double time;
+
+public:
+  PajeUserEvent (PajeContainer *container, PajeType *type, std::string name, PajeEvent *event);
+  void setEvent (PajeEvent *event);
+};
+
+class PajeUserVariable : public PajeEntity
+{
+public:
+  double val;
+  double stime;
+  double etime;
+
+  double startTime (void);
+  double endTime (void);
+  double time (void);
+  double doubleValue (void);
+  double maxValue (void);
+  double minValue (void);
+
+  PajeUserVariable (PajeContainer *container, PajeType *type, double value, double startTime, double endTime);
+  void setEndTime (double endTime);
+};
+
+class PajeUserState : public PajeUserEvent
+{
+public:
+  double endTime;
+  int imbricationLevel;
+  double innerDuration;
+
+public:
+  PajeUserState (PajeContainer *container, PajeType *type, std::string value, PajeEvent *startEvent);
+  void setEndEvent (PajeEvent *event);
+};
+
+class PajeUserLink : public PajeUserState
+{
+public:
+  std::string key;
+  PajeContainer *startContainer;
+  PajeContainer *endContainer;
+
+public:
+  PajeUserLink (PajeContainer *container, PajeType *type, std::string value, std::string key, PajeContainer *startContainer, PajeEvent *startEvent);
+//  PajeUserLink (PajeContainer *container, PajeType *type, std::string value, std::string key, PajeContainer *endContainer, PajeEvent *endEvent);
+};
+
+#include "PajeContainer.h"
+#endif
