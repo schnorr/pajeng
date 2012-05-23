d6428b43d0e94a2d6671aba07f498c40239a993c
schnorr <Lucas.Schnorr@imag.fr>
2012-05-23 17:11:10 +0200

    only get links of types given by the user in the graph configuration

--- src/viva/VivaGraph.cc
+++ src/viva/VivaGraph.cc
@@ -84,7 +84,7 @@ void VivaGraph::defineEdges (PajeContainer *container)
   std::vector<PajeType*>::iterator type;
   for (type = types.begin(); type != types.end(); type++){
     PajeLinkType *link_type = dynamic_cast<PajeLinkType*>(*type);
-    if (link_type){
+    if (link_type && edgeTypes.count (link_type->name)){
       std::vector<PajeEntity*> links;
       links = enumeratorOfEntitiesTypedInContainer (*type, container, startTime(), endTime());
       std::vector<PajeEntity*>::iterator link;
