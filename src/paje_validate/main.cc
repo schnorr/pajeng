79b8b200292d2ba06b17c998d1a2203b8be4c22d
dosimont <damien.dosimont@imag.fr>
2012-06-12 00:01:28 +0200

    Check if argv[1] is not empty

--- src/paje_validate/main.cc
+++ src/paje_validate/main.cc
@@ -5,8 +5,15 @@
 #include "PajeEventDecoder.h"
 #include "PajeSimulator.h"
 
+using namespace std;
+
 int main (int argc, char **argv)
 {
+  if (argc != 2){
+    cerr << "usage : paje_validate [trace-file]" <<endl;
+    return 1;
+    }
+
   PajeFileReader *reader = new PajeFileReader (std::string(argv[1]), NULL);
   PajeEventDecoder *decoder = new PajeEventDecoder ();
   PajeSimulator *simulator = new PajeSimulator ();
@@ -25,7 +32,7 @@ int main (int argc, char **argv)
       i++;
     }
   }catch (std::string exception){
-    std::cout << exception << std::endl;
+    cout << exception << endl;
     return 1;
   }
 
