175e4847d87349960ea3919b68872146a44e2c53
schnorr <Lucas.Schnorr@imag.fr>
2012-06-13 23:48:25 +0200

    paje_interrogate (not ready yet)

---
+++ src/paje_interrogate/main.cc
@@ -0,0 +1,58 @@
+#include <string>
+#include <iostream>
+#include <exception>
+#include "PajeFileReader.h"
+#include "PajeEventDecoder.h"
+#include "PajeSimulator.h"
+
+using namespace std;
+
+bool is_readable (const string & file)
+{
+  ifstream fichier(file.c_str());
+  return !fichier.fail();
+}
+
+int main (int argc, char **argv)
+{
+  if (argc != 2){
+    cerr << "usage : paje_validate [trace-file]" <<endl;
+    return 1;
+  }
+  if (!is_readable(string(argv[1]))){
+    cerr << "trace file \"" << argv[1] << "\" not found" <<endl;
+    return 1;
+  }
+
+  PajeFileReader *reader = new PajeFileReader (std::string(argv[1]), NULL);
+  PajeEventDecoder *decoder = new PajeEventDecoder ();
+  PajeSimulator *simulator = new PajeSimulator ();
+
+  reader->setOutputComponent (decoder);
+  decoder->setInputComponent (reader);
+  decoder->setOutputComponent (simulator);
+  simulator->setInputComponent (decoder);
+
+  try {
+    int i = 0;
+    while (reader->hasMoreData()){
+      reader->startChunk (i);
+      reader->readNextChunk();
+      reader->endOfChunkLast (!reader->hasMoreData());
+      i++;
+    }
+  }catch (std::string exception){
+    cout << exception << endl;
+    return 1;
+  }
+
+  simulator->report();
+
+  //interrogate
+  
+
+  delete reader;
+  delete decoder;
+  delete simulator;
+  return 0;
+}
