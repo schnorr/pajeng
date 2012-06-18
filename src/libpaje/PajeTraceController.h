ad5c56c33d51a58d3654d29a952d390192cb31d5
schnorr <Lucas.Schnorr@imag.fr>
2012-06-18 14:33:48 +0200

    GPL over LGPL

--- src/libpaje/PajeTraceController.h
+++ src/libpaje/PajeTraceController.h
@@ -2,16 +2,16 @@
     This file is part of Paje.
 
     Paje is free software: you can redistribute it and/or modify it
-    under the terms of the GNU Lesser Public License as published by
+    under the terms of the GNU Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
     Paje is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser Public License for more details.
+    GNU Public License for more details.
 
-    You should have received a copy of the GNU Lesser Public License
+    You should have received a copy of the GNU Public License
     along with Paje. If not, see <http://www.gnu.org/licenses/>.
 */
 #ifndef __PAJETRACECONTROLLER_H__
