8e36f42da86836388ba7760217e312c8512e282a
schnorr <Lucas.Schnorr@imag.fr>
2012-06-18 16:30:47 +0200

    use argp to parse

--- src/paje_validate/main.cc
+++ src/paje_validate/main.cc
@@ -4,6 +4,45 @@
 #include "PajeFileReader.h"
 #include "PajeEventDecoder.h"
 #include "PajeSimulator.h"
+#include <argp.h>
+
+#define VALIDATE_INPUT_SIZE 2
+static char doc[] = "Checks if <paje.trace> strictly follows the Paje file format definition";
+static char args_doc[] = "<paje.trace>";
+
+static struct argp_option options[] = {
+  { 0 }
+};
+
+struct arguments {
+  char *input[VALIDATE_INPUT_SIZE];
+  int input_size;
+};
+
+static int parse_options (int key, char *arg, struct argp_state *state)
+{
+  struct arguments *arguments = (struct arguments*)(state->input);
+  switch (key){
+  case ARGP_KEY_ARG:
+    if (arguments->input_size == VALIDATE_INPUT_SIZE) {
+      /* Too many arguments. */
+      argp_usage (state);
+    }
+    arguments->input[state->arg_num] = arg;
+    arguments->input_size++;
+    break;
+  case ARGP_KEY_END:
+    if (state->arg_num < 1) {
+      /* Not enough arguments. */
+      argp_usage (state);
+    }
+    break;
+  default: return ARGP_ERR_UNKNOWN;
+  }
+  return 0;
+}
+
+static struct argp argp = { options, parse_options, args_doc, doc };
 
 bool is_readable (const std::string & filename)
 {
@@ -15,16 +54,21 @@ bool is_readable (const std::string & filename)
 
 int main (int argc, char **argv)
 {
-  if (argc != 2){
-    std::cerr << "usage : paje_validate [trace-file]" << std::endl;
+  struct arguments arguments;
+  bzero (&arguments, sizeof(struct arguments));
+  if (argp_parse (&argp, argc, argv, 0, 0, &arguments) == ARGP_KEY_ERROR){
+    fprintf(stderr, "%s, error during the parsing of parameters\n", argv[0]);
     return 1;
   }
-  if (!is_readable(std::string(argv[1]))){
+
+  if (!is_readable(std::string(arguments.input[0]))){
     std::cerr << "trace file \"" << argv[1] << "\" not found" << std::endl;
     return 1;
   }
 
-  PajeFileReader *reader = new PajeFileReader (std::string(argv[1]), NULL);
+  std::cout << arguments.input[0] << std::endl;
+
+  PajeFileReader *reader = new PajeFileReader (std::string(arguments.input[0]), NULL);
   PajeEventDecoder *decoder = new PajeEventDecoder ();
   PajeSimulator *simulator = new PajeSimulator ();
 
