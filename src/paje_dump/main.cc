0201932e108c99ec9803485a109401b3a38642bf
schnorr <Lucas.Schnorr@imag.fr>
2012-06-18 16:34:47 +0200

    argp + GPL to pj_dump

--- src/paje_dump/main.cc
+++ src/paje_dump/main.cc
@@ -1,9 +1,62 @@
+/*
+    This program is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <string>
 #include <iostream>
 #include <exception>
 #include "PajeFileReader.h"
 #include "PajeEventDecoder.h"
 #include "PajeSimulator.h"
+#include <argp.h>
+
+#define VALIDATE_INPUT_SIZE 2
+static char doc[] = "Dumps the <paje.trace> file in a CSV-like textual format";
+static char args_doc[] = "<paje.trace>";
+
+static struct argp_option options[] = {
+  { 0 }
+};
+
+struct arguments {
+  char *input[VALIDATE_INPUT_SIZE];
+  int input_size;
+};
+
+static int parse_options (int key, char *arg, struct argp_state *state)
+{
+  struct arguments *arguments = (struct arguments*)(state->input);
+  switch (key){
+  case ARGP_KEY_ARG:
+    if (arguments->input_size == VALIDATE_INPUT_SIZE) {
+      /* Too many arguments. */
+      argp_usage (state);
+    }
+    arguments->input[state->arg_num] = arg;
+    arguments->input_size++;
+    break;
+  case ARGP_KEY_END:
+    if (state->arg_num < 1) {
+      /* Not enough arguments. */
+      argp_usage (state);
+    }
+    break;
+  default: return ARGP_ERR_UNKNOWN;
+  }
+  return 0;
+}
+
+static struct argp argp = { options, parse_options, args_doc, doc };
 
 bool is_readable (const std::string & filename)
 {
@@ -57,11 +110,14 @@ void dump (PajeSimulator *simulator)
 
 int main (int argc, char **argv)
 {
-  if (argc < 2){
-    std::cerr << "usage : " << argv[0] << " <trace-file>" << std::endl;
+  struct arguments arguments;
+  bzero (&arguments, sizeof(struct arguments));
+  if (argp_parse (&argp, argc, argv, 0, 0, &arguments) == ARGP_KEY_ERROR){
+    fprintf(stderr, "%s, error during the parsing of parameters\n", argv[0]);
     return 1;
   }
-  if (!is_readable(std::string(argv[1]))){
+
+  if (!is_readable(std::string(arguments.input[0]))){
     std::cerr << "trace file \"" << argv[1] << "\" not found" << std::endl;
     return 1;
   }
