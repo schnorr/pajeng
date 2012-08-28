cfe9aa2b6dc9aa8f22540224617eb79ca67da8c1
schnorr <Lucas.Schnorr@imag.fr>
2012-08-29 00:39:15 +0200

    apply gpl license to all files

--- src/libpaje/PajeEntity.h
+++ src/libpaje/PajeEntity.h
@@ -1,18 +1,18 @@
 /*
-    This file is part of Paje.
+    This file is part of PajeNG
 
-    Paje is free software: you can redistribute it and/or modify it
-    under the terms of the GNU Public License as published by
+    PajeNG is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Paje is distributed in the hope that it will be useful,
+    PajeNG is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Public License for more details.
 
     You should have received a copy of the GNU Public License
-    along with Paje. If not, see <http://www.gnu.org/licenses/>.
+    along with PajeNG. If not, see <http://www.gnu.org/licenses/>.
 */
 #ifndef __PAJE_ENTITY_H
 #define __PAJE_ENTITY_H
