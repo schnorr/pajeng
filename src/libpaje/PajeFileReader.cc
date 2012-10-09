e481fa7666bbd150fdc81b0c0dc4ffcbb789b542
schnorr <Lucas.Schnorr@imag.fr>
2012-10-09 10:47:02 +0200

    remove gettime useless code

--- src/libpaje/PajeFileReader.cc
+++ src/libpaje/PajeFileReader.cc
@@ -17,13 +17,6 @@
 #include "PajeFileReader.h"
 #include <sys/time.h>
 
-double gettime ()
-{
-  struct timeval tr;
-  gettimeofday(&tr, NULL);
-  return (double)tr.tv_sec+(double)tr.tv_usec/1000000;
-}
-
 PajeFileReader::PajeFileReader (std::string f)
 {
   chunkSize = PAJE_DEFAULT_CHUNK_SIZE;
