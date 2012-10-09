9416bae7a14a01e8270ebeb3822edcc7599b93df
schnorr <Lucas.Schnorr@imag.fr>
2012-10-09 12:37:35 +0200

    PajeThreadReader following new protocol for reading files

--- src/utils/PajeThreadReader.cc
+++ src/utils/PajeThreadReader.cc
@@ -34,15 +34,14 @@ void PajeThreadReader::read ()
 
 void PajeThreadReader::run ()
 {
+  reader->startReading();
   while(!isFinished() && reader->hasMoreData()){
-    static int i = 0;
-    reader->startChunk (i++);
     reader->readNextChunk();
-    reader->endOfChunkLast (!reader->hasMoreData());
     double percentage = (double)reader->traceRead()/(double)reader->traceSize()*100; 
     fprintf (stderr, "%f\r", percentage);
     emit percentageCompleted(percentage);
   }
+  reader->finishedReading();
   emit percentageCompleted(100);
   emit closeDialogIfStillOpen ();
 }
