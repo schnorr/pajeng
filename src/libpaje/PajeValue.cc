4443078cf6e409ddf33b18ec7406ed2e14ad7bcb
Lucas Schnorr <Lucas.Schnorr@imag.fr>
2012-10-23 09:34:50 +0200

    new class to implement a value

---
+++ src/libpaje/PajeValue.cc
@@ -0,0 +1,45 @@
+/*
+    This file is part of PajeNG
+
+    PajeNG is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    PajeNG is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Public License for more details.
+
+    You should have received a copy of the GNU Public License
+    along with PajeNG. If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "PajeValue.h"
+
+PajeValue::PajeValue (std::string name, std::string alias, PajeType *type, PajeColor *color)
+{
+  _color = color;
+  _name = name;
+  _alias = alias;
+  _type = type;
+}
+
+PajeColor *PajeValue::color (void)
+{
+  return _color;
+}
+
+std::string PajeValue::name (void)
+{
+  return _name;
+}
+
+std::string PajeValue::alias (void)
+{
+  return _alias;
+}
+
+PajeType *PajeValue::type (void)
+{
+  return _type;
+}
